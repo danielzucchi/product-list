20191c757c27cd0567d03189260900eee62dee99
Daniel Zucchi <danielzucchi@Daniels-MacBook-Pro.local>
2020-11-18 22:14:26 +0000

    Delete unneeded service

--- src/services/fetchProducts.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const fetchProducts = () =>
-  axios
-    .get('https://5c78274f6810ec00148d0ff1.mockapi.io/api/v1/products')
-    .then((response) => response)
-
-export default fetchProducts
